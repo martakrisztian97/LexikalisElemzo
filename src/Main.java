c0810dbac2b6bcacfadcb0e1c970f67e1ac5e5d9
Márta Krisztián <martakrisztian97@gmail.com>
2022-02-22 20:35:00 +0100

    Tokenize (****) comment, ignore non-token characters.

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,44 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-        //String domol = new String("12domosi12:=(**domosi**){domosi}>=domosi");
-        String domol = new String("12sa12a1ss12as21");
+        StringBuffer domol = new StringBuffer("12domosi12:=(**domosi**){domosi}>12=domosi");
+        //StringBuffer domol = new StringBuffer("12domosi12(**domosi**)kfff");
         List<String> tokens = new ArrayList<>();
         Pattern pattern;
         Matcher m;
+        boolean find;
 
         while (domol.length() != 0) {
+            find = false;
             if (Character.isDigit(domol.charAt(0))) {
                 pattern = Pattern.compile("[0-9]+");
                 m = pattern.matcher(domol);
                 if (m.find()) {
                     tokens.add(m.group());
+                    find = true;
                 }
             } else if (Character.isLetter(domol.charAt(0))) {
                 pattern = Pattern.compile("[a-zA-Z]+[0-9]+|[a-zA-Z]+");
                 m = pattern.matcher(domol);
                 if (m.find()) {
                     tokens.add(m.group());
+                    find = true;
                 }
+            } else if (domol.charAt(0) == '(') {
+                pattern = Pattern.compile("\\(\\*\\*.*\\*\\*\\)");
+                m = pattern.matcher(domol);
+                if (m.find()) {
+                    tokens.add(m.group());
+                    find = true;
+                }
+            } else {
+                domol.delete(0, 1);
             }
-            domol = domol.replaceFirst(tokens.get(tokens.size()-1), "");
+
+            if (find) {
+                domol.delete(0, tokens.get(tokens.size()-1).length());
+            }
+
             System.out.println(domol+" ");
         }
 
