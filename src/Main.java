f3127162ebefed0eaaa4d38496743045d3d8d3f4
Márta Krisztián <martakrisztian97@gmail.com>
2022-02-26 00:02:43 +0100

    Create Token class and fix List<String> to List<Token>

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,27 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+    public static class Token {
+        private String token;
+        private String type;
+
+        public String getToken() {
+            return token;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public Token(String token, String type) {
+            this.token = token;
+            this.type = type;
+        }
+    }
+
     public static void main(String[] args) {
         StringBuffer domol = new StringBuffer("12domosi12:=(**domosi**){domosi}>=domosi$");
-        List<String> tokens = new ArrayList<>();  // lexémák/tokenek listája
+        List<Token> tokens = new ArrayList<>();  // lexémák/tokenek listája
         Pattern pattern;
         Matcher m;
         boolean find;
@@ -19,7 +37,7 @@ public class Main {
                 pattern = Pattern.compile("[0-9]+");
                 m = pattern.matcher(domol);
                 if (m.find()) {
-                    tokens.add(m.group());
+                    tokens.add(new Token(m.group(), "<előjelnélküli egész>"));
                     find = true;
                 }
             // <azonosító>
@@ -27,7 +45,7 @@ public class Main {
                 pattern = Pattern.compile("[a-zA-Z]+[0-9]+|[a-zA-Z]+");
                 m = pattern.matcher(domol);
                 if (m.find()) {
-                    tokens.add(m.group());
+                    tokens.add(new Token(m.group(), "<azonosító>"));
                     find = true;
                 }
             // <(**) kommentár>
@@ -35,7 +53,7 @@ public class Main {
                 pattern = Pattern.compile("\\(\\*\\*.*\\*\\*\\)");
                 m = pattern.matcher(domol);
                 if (m.find()) {
-                    tokens.add(m.group());
+                    tokens.add(new Token(m.group(), "<(**) kommentár>"));
                     find = true;
                 }
             // <{} kommentár>
@@ -43,43 +61,44 @@ public class Main {
                 pattern = Pattern.compile("\\{.*\\}");
                 m = pattern.matcher(domol);
                 if (m.find()) {
-                    tokens.add(m.group());
+                    tokens.add(new Token(m.group(), "<{} kommentár>"));
                     find = true;
                 }
             // <értékadás>
             } else if (domol.charAt(0) == ':' && domol.charAt(1) == '=') {
-                tokens.add(":=");
+                tokens.add(new Token(":=", "<értékadás>"));
                 find = true;
             // <nagyobbegyenlő>
             } else if (domol.charAt(0) == '>' && domol.charAt(1) == '=') {
-                tokens.add(">=");
+                tokens.add(new Token(">=", "<nagyobbegyenlő>"));
                 find = true;
             // <kisebbegyenlő>
             } else if (domol.charAt(0) == '<' && domol.charAt(1) == '=') {
-                tokens.add("<=");
+                tokens.add(new Token("<=", "<kisebbegyenlő>"));
                 find = true;
             // <nemegyenlő>
             } else if (domol.charAt(0) == '<' && domol.charAt(1) == '>') {
-                tokens.add("<>");
+                tokens.add(new Token("<>", "<nemegyenlő>"));
                 find = true;
             // <eof>
             } else if (domol.charAt(0) == '$') {
-                tokens.add("$");
+                tokens.add(new Token("$", "<eof>"));
                 find = true;
             } else {
                 domol.delete(0, 1);
             }
 
             if (find) {
-                domol.delete(0, tokens.get(tokens.size()-1).length());
+                domol.delete(0, tokens.get(tokens.size()-1).getToken().length());
             }
 
             System.out.println(domol+" ");
         }
 
         System.out.println("tokens:");
-        for (String token : tokens) {
-            System.out.println(token);
+        for (Token token : tokens) {
+
+            System.out.println(token.getToken()+"\t"+token.getType());
         }
     }
 }
